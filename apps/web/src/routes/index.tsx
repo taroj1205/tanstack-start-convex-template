773600157e85523d2fe70b7250d45ac6cc435ebd
Shintaro Jokagi <taroj1205@gmail.com>
2025-07-29 14:53:17 +1200

    chore(convex): update convex structure

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { convexQuery } from "@convex-dev/react-query";
+import { api } from "@repo/convex/api";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { Container, Text, VStack } from "@yamada-ui/react";
-import { api } from "backend/convex/_generated/api";
 
 export const Route = createFileRoute("/")({
   component: HomeComponent,
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,7 @@
-import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    tailwindcss(),
-    tanstackStart({ target: "cloudflare-module" }),
-  ],
+  plugins: [tsconfigPaths(), tanstackStart({ target: "cloudflare-module" })],
 });
